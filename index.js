cdb9f9cebdceb9ba70ac40817e9acbb7ed31d8a5
Porter Hoskins <porter@route.com>
2021-10-08 19:23:53 -0600

    Folder

--- index.js
+++ index.js
@@ -12,38 +12,47 @@ knex = require("knex")(
         client: "mysql",
         connection:
         {
-            host: "localhost", // Insert the read URL
-            user: "admin", //
-            password: "password1",
-            database: "icecreamdonuts",
+            host: "rmic5stpzqgokb.cgxk7v9iabjl.us-west-2.rds.amazonaws.com", // Insert the read URL
+            user: "DBUsername", //
+            password: "DBPassword",
+            database: "MyDatabase",
             port: 3306,
         },
     });
 
 app.get("/", async (req, res) => {
     // UNCOMMENT THIS WHEN THE DB CONNECTION INFORMATION IS CORRECT/SET UP
-    // const result = await knex.select().from("flavor")
-    // console.log(result);
-    // res.send(result);
-    res.render("index");
+    const result = await knex.select().from("flavor")
+    console.log(result);
+    res.send(result);
+    // res.render("index");
 });
 
 app.get("/donuts", async (req, res) => {
     // UNCOMMENT THIS WHEN THE DB CONNECTION INFORMATION IS CORRECT/SET UP
-    // const result = await knex.select().from("flavor")
-    const result = donuts // Delete when database working
+    const result = await knex.select().from("flavor")
+    // const result = donuts // Delete when database working
     console.log(result);
     res.setHeader('Content-Type', 'application/json')
     res.send(JSON.stringify(result))
 });
 
-app.get("/load", (req, res) => {
+app.get("/load", async (req, res) => {
+    await knex.schema.createTableIfNeeded("flavor", (table) => {
+        table.increments()
+        table.string("name")
+        table.string("category")
+        table.string("imageURL")
+        table.string("donutID")
+    })
+
     res.setHeader('Content-Type', 'application/json')
+
     res.end(JSON.stringify(knex("flavor").insert(donuts).returning('*')))
 });
 
 app.get("/nuke", async (req, res) => {
-    knex('flavors').del()
+    knex.schema.dropTable('flavor')
     res.send(JSON.stringify({ message: "deleted" }))
 });
 
